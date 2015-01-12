677145582d6b22e705fb08527f95fbd284161ca0
Tobias Kremer <tobias.kremer@comsysto.com>
2015-01-12 08:56:09 +0100

    integrated SpringBootServletInitializer into main class

--- src/main/java/com/comsysto/wicketBoot/ApplicationStarter.java
+++ src/main/java/com/comsysto/wicketBoot/ApplicationStarter.java
@@ -2,12 +2,14 @@ package com.comsysto.wicketBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Main Class of the Application used to bring up the application Context.ƒ
  */
 @SpringBootApplication
-public class ApplicationStarter {
+public class ApplicationStarter extends SpringBootServletInitializer {
 
 	/**
 	 * Start Spring Boot Application using Command Line Args.
@@ -17,5 +19,10 @@ public class ApplicationStarter {
 	 */
 	public static void main(final String... args) {
 		SpringApplication.run(ApplicationStarter.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(ApplicationStarter.class);
 	}
 }
